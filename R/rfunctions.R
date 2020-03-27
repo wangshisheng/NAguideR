1e789511e36f24125f23c7522b8ae0fb1bc9ffe4
wangshisheng <wssdandan2009@outlook.com>
2020-03-27 12:50:14 +0800

    Update rfunctions.R

--- R/rfunctions.R
+++ R/rfunctions.R
@@ -1,4 +1,4 @@
-#' Runs the motifeR Shiny web application.
+#' Run the motifeR Shiny web application locally.
 #' @export
 NAguideR_app <- function() {
   shiny::runApp(system.file('NAguideRapp', package='NAguideR'),
